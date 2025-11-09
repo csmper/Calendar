d4346d140d7cf852ce57cfc5e1a1d15370079dec
Suhas <suhas.gavas@gmail.com>
2025-11-09 23:05:46 +0530

    style changes and app icon

--- CalendarWidget/CalendarWidget.swift
+++ CalendarWidget/CalendarWidget.swift
@@ -40,34 +40,30 @@ struct DayEntry: TimelineEntry {
 
 struct CalendarWidgetEntryView : View {
     var entry: DayEntry
-    var config: MonthConfig
     
     init(entry: DayEntry) {
         self.entry = entry
-        self.config = MonthConfig.determineConfig(for: entry.date)
     }
 
     var body: some View {
         ZStack {
             VStack {
                 HStack(spacing: 4) {
-                    Text(config.emojiText)
-                        .font(.title)
                     Text(entry.date.weekdayDisplayFormat)
                         .font(.title3)
                         .fontWeight(.bold)
                         .minimumScaleFactor(0.5)
-                        .foregroundColor(config.weekdayTextColor)
+                        .foregroundColor(.yellow)
                     Spacer()
                 }
                 
                 Text(entry.date.dayDisplayFormat)
                     .font(.system(size: 80, weight: .heavy))
-                    .foregroundColor(config.dayTextColor)
+                    .foregroundColor(.white)
             }
             .padding(2)
         }
-        .containerBackground(config.backgroundColor.gradient, for: .widget)
+        .containerBackground(Color(hex: "#5271ff"), for: .widget)
     }
 }
 
@@ -81,6 +77,32 @@ struct CalendarWidget: Widget {
         .configurationDisplayName("Suhas Calendar Widget")
         .description("A simple widget to display day of the Month")
         .supportedFamilies([.systemSmall])
+    }
+}
+
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int = UInt64()
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (255, 0, 0, 0)
+        }
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue:  Double(b) / 255,
+            opacity: Double(a) / 255
+        )
     }
 }
 
